6f23274d2298f58ebcbf3cf526ad8bf1de57e256
Jordy Meow <inscr@meow.fr>
2015-01-29 12:20:39 +0900

    Default array in case of null avoids a lot of 'notices'.

--- lib/ManualImageCropSettingsPage.php
+++ lib/ManualImageCropSettingsPage.php
@@ -148,7 +148,7 @@ class MicSettingsPage
 			 </thead>
              <tbody>';
 		
-		$sizesSettings = self::getSettings();
+		$sizesSettings = self::getSettings() || array();
 		
 		foreach ($imageSizes as $s) {
 			$label = isset($sizeLabels[$s]) ? $sizeLabels[$s] : ucfirst( str_replace( '-', ' ', $s ) );
