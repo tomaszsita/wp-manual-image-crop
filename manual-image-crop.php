84a18e99a143e4f655c6687715fb77539cb03d71
Radley Sustaire <radley@limelightdept.com>
2015-12-02 15:44:45 -0800

    Updated version again. Intentionally not WordPress.org compliant.

--- manual-image-crop.php
+++ manual-image-crop.php
@@ -3,7 +3,7 @@
 Plugin Name: Manual Image Crop
 Plugin URI: https://github.com/tomaszsita/wp-manual-image-crop
 Description: Plugin allows you to manually crop all the image sizes registered in your WordPress theme (in particular featured image). Simply click on the "Crop" link next to any image in your media library and select the area of the image you want to crop.
-Version: 1.12r1
+Version: 1.12r2
 Author: Tomasz Sita
 Author URI: https://github.com/tomaszsita
 License: GPL2
@@ -11,7 +11,7 @@ Text Domain: microp
 Domain Path: /languages/
 */
 
-define('mic_VERSION', '1.12r1');
+define('mic_VERSION', '1.12r2');
 
 include_once(dirname(__FILE__) . '/lib/ManualImageCropSettingsPage.php');
 
